76f0e16a084e6cf3b11ef936e6f07a90a2821b88
Summer3185 <nnhtkf@gmail.com>
2026-01-28 12:52:14 +0700

    Add files via upload

---
+++ OOP_B1_281/Program.cs
@@ -0,0 +1,35 @@
+﻿using OOP_1;
+using System;
+using System.Text;
+
+namespace OOP_B1_281
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+           
+            Sinhvien[] dsSV = new Sinhvien[2];
+
+           
+            Console.WriteLine("--- NHẬP THÔNG TIN SINH VIÊN ---");
+            for (int i = 0; i < 2; i++)
+            {
+                Console.WriteLine($"Nhập sinh viên thứ {i + 1}:");
+                dsSV[i] = new Sinhvien(); 
+                dsSV[i].NhapSV();
+            }
+
+           
+            Console.WriteLine("\n--- Hien thi thong tin sinh vien ---");
+            for (int i = 0; i < 2; i++)
+            {
+                dsSV[i].DisplaySV(); 
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ OOP_B1_281/Sinhvien.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace OOP_B1_281
+{
+    internal class Sinhvien
+    {
+        public string Msv;
+        public string HoTen;
+        public int Tuoi;
+        public Sinhvien() { } 
+
+        public Sinhvien(string Msv, string HoTen, int Tuoi)
+        {
+            this.Msv = Msv;
+            this.HoTen = HoTen;
+            this.Tuoi = Tuoi;
+        }
+        public void NhapSV()
+        {
+            Console.Write("Nhập Mã sinh viên: ");
+            Msv = Console.ReadLine();
+            Console.Write("Nhập Họ tên: ");
+            HoTen = Console.ReadLine();
+            Console.Write("Nhập Tuổi: ");
+            Tuoi = int.Parse(Console.ReadLine());
+        }
+
+        public void DisplaySV()
+        {
+            Console.WriteLine("Mã sinh viên: {0} | Họ tên: {1} | Tuổi: {2}", Msv, HoTen, Tuoi);
+        }
+    }
+}
+
